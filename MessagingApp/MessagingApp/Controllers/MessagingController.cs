a6e8b223e5350c3a7f28affbc36a94c179f98c2d
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-04-07 22:33:07 +0300

    init

---
+++ MessagingApp/MessagingApp/Controllers/MessagingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessagingController : Controller
+    {
+        private readonly ILogger<MessagingController> _logger;
+
+        public MessagingController(ILogger<MessagingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}", Name = "GetMessage")]
+        public string Get(string id)
+        {
+            return "You successfuly pulled a message!";
+        }
+
+        [HttpPost("send")]
+        public string PostMessage(string message)
+        {
+            return message;
+        }
+    }
+}
