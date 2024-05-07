acde01b006d9837250a8e2a6bba2c3083a883ff5
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-05-07 20:43:31 +0300

    Delete MessagingAppUI/messagingappui directory

--- MessagingAppUI/messagingappui/src/components/error/error.jsx
+++
@@ -1,9 +0,0 @@
-function Error({ message }) {
-    return (
-        <div className={"mx-auto w-50 text-danger"}>
-            <span>{message}</span>
-        </div>
-    );
-}
-
-export default Error;
