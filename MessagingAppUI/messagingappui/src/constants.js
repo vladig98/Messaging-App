c83083b5ace3d937dc2bef55e4f7b48f5dad283e
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-05-07 21:17:27 +0300

    push

---
+++ MessagingAppUI/messagingappui/src/constants.js
@@ -0,0 +1,17 @@
+export const JWT_STORAGE_NAME = "JWTAuthToken";
+export const UsersStorageName = "UsersInfo";
+export const domainName = "https://localhost:7238";
+export const METHODS = {
+    GET: "GET",
+    POST: "POST"
+};
+export const ROUTES = {
+    HOME: '/',
+    LOGIN: '/login',
+    REGISTER: '/register',
+    LOGOUT: '/logout',
+    GETUSERS: '/getusers',
+    CHAT_ID: '/chat/:id',
+    GETUSERINFO: '/getuserinfo',
+    GETCHATINFO: '/getchatinfo'
+};
